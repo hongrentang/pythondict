e99bf095600969b67e64daf95e5800b8ae0ee10b
hongrentang <370603297@qq.com>
2018-06-04 14:48:21 +0800

    修改

--- Code/实验楼-Flask Web 应用 BUG 修复-代码/Code/ele.py
+++ Code/实验楼-Flask Web 应用 BUG 修复-代码/Code/ele.py
@@ -6,7 +6,7 @@ import requests
 
 def ele_red_packet(number):
     s = requests.session()
-    url = "http://m.quanmama.com/mzdm/2111914.html"
+    url = "http://www.quanmama.com/quan/2362429.html"
     user_agent = 'User-Agent: Mozilla/5.0 (Linux; Android 7.1.1; MI 6 Build/NMF26X; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36 MicroMessenger/6.5.13.1081 NetType/WIFI Language/zh_CN'
     headers = {'User-Agent': user_agent}
     s1 = r"group_sn=\w{32}"
@@ -26,21 +26,21 @@ def ele_red_packet(number):
 
     phone = number
 
-    sn='10db3582b00f00a1'
-
     s3_list = []
-    for url in s2:
+    for sn in s2:
         value = {
-            "group_sn": url[9:],
+            "group_sn": sn[9:],
             "phone": phone,
             "weixin_uid": '468015ki5tulqs9mbjmjvr6w83o45kh9'
         }
         s3 = s.post("https://restapi.ele.me/marketing/hongbao/h5/grab",
                     json=value, headers=headers)
 
-        s3_list.append(s3.status_code)
+        if "hongbao_list" in s3.text:
+            s3_list.append(s3.status_code)
+
 
     if 200 in s3_list:
-        return u"领取成功，{len(s3_list)} 个红包已注入 {phone} 的饿了么账户!"
+        return f"领取成功，{len(s3_list)} 个红包已注入 {phone} 的饿了么账户!"
     else:
         return "红包领取失败！"
--- Code/实验楼-Flask Web 应用 BUG 修复-代码/Code/web_run.py
+++ Code/实验楼-Flask Web 应用 BUG 修复-代码/Code/web_run.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 from flask import request
@@ -10,13 +9,21 @@ from ele import ele_red_packet
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/',methods=['GET','POST'])
 def phone_number_form():
+    if request.method == 'GET':
+        return render_template('index.html')
+
     if request.method == 'POST':
         phone_number = request.form['phone']
-        get_red_packet = ele_red_packet(phone_number)
-    return render_template('index.html',phone_number=get_red_packet)
+        if phone_number:
+            get_red_packet = ele_red_packet(phone_number)
+            return render_template('index.html', phone_number=get_red_packet)
+        else:
+            return render_template('index.html', phone_number='手机号不能为空')
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
